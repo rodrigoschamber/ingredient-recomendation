6122a74f06cedd5d4d3efc35d031986fb566cbce
Rodrigo Schamber <rodrigoschamber@gmail.com>
2024-09-30 15:20:59 -0300

    add endpoint

--- backend/main.go
+++ backend/main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
-	"fmt"
-
 	"backend/builder"
+	"encoding/json"
+	"fmt"
+	"net/http"
 )
 
 func main() {
     concreteBuilder := &builder.ConcreteBuilder{}
     director := builder.NewDirector(concreteBuilder)
     director.Construct()
-    product := concreteBuilder.GetResult()
-    fmt.Printf("Product: %+v\n", product)
+    ingredient := concreteBuilder.GetResult()
+	http.HandleFunc("/ingredient", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(ingredient)
+	})
+
+	fmt.Println("Server is running on port 8080")
+	http.ListenAndServe(":8080", nil)
+    fmt.Printf("Product: %+v\n", ingredient)
 }
