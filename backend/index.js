f5d5fbf0b387e2ecbbfeb87933f7fcf6c8caa13c
Rodrigo Schamber <rodrigoschamber@gmail.com>
2024-10-03 14:08:13 -0300

    fix

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const builder = new ActiveIngredientBuilder();
 const director = new ActiveIngredientDirector(builder);
 
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
