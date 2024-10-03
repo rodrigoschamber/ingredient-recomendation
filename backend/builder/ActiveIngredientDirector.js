82303b2131fcc9ec9a0312f9f0cb82e3ff4b4e62
Rodrigo Schamber <rodrigoschamber@gmail.com>
2024-10-03 20:57:51 -0300

    add new prompt

--- backend/builder/ActiveIngredientDirector.js
+++ backend/builder/ActiveIngredientDirector.js
@@ -1,6 +1,9 @@
 import PromptFacade from "../facade/promptFacade.js";
 import JsonRepository from "../repository/JsonRepository.js";
-import { getDiseasePrompt, getNamePrompt } from "../prompts/prompts.js";
+import {
+  getIngredientByDisease,
+  getIngredientByName,
+} from "../prompts/prompts.js";
 
 const repository = new JsonRepository("./data/stock.json");
 
@@ -10,7 +13,7 @@ class ActiveIngredientDirector {
   }
 
   async constructByDisease(disease) {
-    const api = new PromptFacade(getDiseasePrompt(disease));
+    const api = new PromptFacade(getIngredientByDisease(disease));
     const response = await api.generateContent();
 
     return response?.map((item) => {
@@ -24,7 +27,7 @@ class ActiveIngredientDirector {
   }
 
   async constructByName(name) {
-    const api = new PromptFacade(getNamePrompt(name));
+    const api = new PromptFacade(getIngredientByName(name));
     const response = await api.generateContent();
     return response?.map((item) => {
       return this.builder
--- backend/prompts/prompts.js
+++ backend/prompts/prompts.js
@@ -1,4 +1,4 @@
-export const getDiseasePrompt = (payload) => {
+export const getIngredientByDisease = (payload) => {
   return {
     text: `Informar os princípios ativos para ${payload} e classificar quanto ao mecanismo de ação em formato json.
           descrição:
@@ -7,7 +7,16 @@ export const getDiseasePrompt = (payload) => {
   };
 };
 
-export const getNamePrompt = (payload) => {
+export const getIngredientList = (payload) => {
+  return {
+    text: `Listar os princípios ativos para o tratamento da ${payload} com uma descrição e mecanismo de ação em formato json.
+          descrição:
+          mecanismo:
+          principio ativo:`,
+  };
+};
+
+export const getIngredientByName = (payload) => {
   return {
     text: `Informar os princípios ativos com o mesmo mecanismo de ação de ${payload} em formato json.
           descrição:
