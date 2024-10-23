a914c78f062fc6dd4eb16365b8eaee2a296f518e
Rodrigo Schamber <rodrigoschamber@gmail.com>
2024-10-23 20:40:56 -0300

    add logo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import IngredientCard from "./components/IngredientCard";
 import IngredientProgress from "./components/IngredientProgress";
 import IngredientSearch from "./components/IngredientSearch";
 import { Box, Container } from "@mui/material";
+import logo from "./assets/logo.png";
 
 export default function App() {
   const [progress, setProgress] = useState(false);
@@ -11,17 +12,31 @@ export default function App() {
 
   return (
     <Container maxWidth="sm">
-      <Box sx={{ my: 4 }}>
-        <IngredientSearch
-          progress={progress}
-          setProgress={setProgress}
-          setResponseData={setResponseData}
+      <Box
+        sx={{
+          my: 4,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <img
+          src={logo}
+          alt="Recomendação de Princípios Ativos"
+          style={{ width: "150px", borderRadius: "75px" }}
         />
-        {progress ? (
-          <IngredientProgress />
-        ) : (
-          <IngredientCard ingredientList={responseData} />
-        )}
+        <Box sx={{ mt: 2 }}>
+          <IngredientSearch
+            progress={progress}
+            setProgress={setProgress}
+            setResponseData={setResponseData}
+          />
+          {progress ? (
+            <IngredientProgress />
+          ) : (
+            <IngredientCard ingredientList={responseData} />
+          )}
+        </Box>
       </Box>
     </Container>
   );
